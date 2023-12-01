669a6c2198caf93c9a2680c4217294baec65fcb2
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2023-12-01 17:23:21 +0100

    dev: console

--- src/routes/api/text2img/+server.ts
+++ src/routes/api/text2img/+server.ts
@@ -3,9 +3,11 @@ import type { RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
 	const { prompt } = await request.json();
+	console.log(prompt);
 	let result = await run('@cf/stabilityai/stable-diffusion-xl-base-1.0', {
 		prompt: prompt
 	});
+	console.log(result);
 	return new Response(result, {
 		headers: {
 			'content-type': 'image/png'
